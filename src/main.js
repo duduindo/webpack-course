9fe1ec5e74a882620f23bebf90caea8e52dcbc71
Eduardo Paixão <eduardo.paixao@grupofolha.com.br>
2018-03-06 13:04:48 -0300

    7º lesson

--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
+require("babel-runtime/regenerator")
 require("./main.css")
 require("./images/link.jpg")
 require("./index.html")
 
-var a = () => {
-  console.log("Hello from the future!")
+var a = async (args) => {	
+	const { a, b } = args;
+  await console.log("Hello from the future!", a, b);
+  console.log('Done');
 }
+
+
+a({a: 1, b: 2})
+/*
+var a = function () {
+  return Promise.resolve().then(function () {    
+    return console.log("Hello from the future!");
+  }).then(function () {
+    console.log('Done');
+  });
+};
+
+*/
